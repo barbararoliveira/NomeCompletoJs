6cee6a40af0b8c8ace8e4479bfe295bdcf842b68
Bárbara <babyr.oliveira@gmail.com>
2020-10-11 14:50:12 -0300

    Resolução final do exercício.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let nome = prompt ("Qual o seu nome?");
-let sobrenome = prompt ("Qual  seu sobrenome?");
+let sobrenome = prompt ("Qual o seu sobrenome?");
 let sobrenomecaps = sobrenome.toUpperCase();
 
 alert (`Nome completo: ${nome} ${sobrenome}.`);
