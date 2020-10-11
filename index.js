6311cafc6bcd848c2736ae6935ea339fb745d935
Bárbara <babyr.oliveira@gmail.com>
2020-10-11 14:47:44 -0300

    Resolução do exercíco.

---
+++ index.js
@@ -0,0 +1,6 @@
+let nome = prompt ("Qual o seu nome?");
+let sobrenome = prompt ("Qual  seu sobrenome?");
+let sobrenomecaps = sobrenome.toUpperCase();
+
+alert (`Nome completo: ${nome} ${sobrenome}.`);
+alert (`Nome de catálogo: ${sobrenomecaps}, ${nome}.`);
